b52e2d80f26def421f0bf0006bd8599668fddceb
Eason0210 <aqua0210@163.com>
2023-01-07 10:53:46 +0800

    Prefer elif syntax and clean code

--- build-emacs.sh
+++ build-emacs.sh
@@ -42,18 +42,18 @@ cd "${ROOT_DIR}/emacs-tarballs"
 
 # Check for valid Git commit hash
 GIT_COMMIT=$1
-if [[ "$GIT_COMMIT" != "master" && "$GIT_COMMIT" != "emacs-29" && ! "$GIT_COMMIT" =~ ^[a-zA-Z0-9]{40}$ ]]; then
-    if [[ -z "$1" ]]; then
-        emacs_src_url="${SRC_BASE_URL}-emacs-29.tar.gz"
-        emacs_src="emacs-emacs-29"
-    else
-        printf "Error! Please give a valid Git commit hash.\n"
-        exit 1
-    fi
-else
+if [[ "$GIT_COMMIT" = "master" || "$GIT_COMMIT" = "emacs-29" || "$GIT_COMMIT" =~ ^[a-zA-Z0-9]{40}$ ]]
+then
     printf "Git commit hash is valid.\n"
     emacs_src_url="${SRC_BASE_URL}-${GIT_COMMIT}.tar.gz"
     emacs_src="emacs-${GIT_COMMIT}"
+elif [[ -z "$1" ]]
+then
+    emacs_src_url="${SRC_BASE_URL}-emacs-29.tar.gz"
+    emacs_src="emacs-emacs-29"
+else
+    printf "Error! Please give a valid Git commit hash.\n"
+    exit 1
 fi
 
 emacs_src_tarball="${ROOT_DIR}/emacs-tarballs/${emacs_src}.tar.gz"
