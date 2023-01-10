e10c70b5a86888b581c11d4c736c4f0c8f8c844d
Eason0210 <aqua0210@163.com>
2023-01-10 21:35:19 +0800

    Update the doc string of emacs-version-git-commit

--- materials/emacs-git-version.el
+++ materials/emacs-git-version.el
@@ -6,7 +6,7 @@
 ;; Modified: Eason Huang
 ;; Keywords: emacs
 ;; Created: 2012-09-21;
-;; Last changed: 2023-01-10 20:09:00
+;; Last changed: 2023-01-10 21:38:00
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -22,14 +22,13 @@
 SHA1 of git commit that was used to compile this version of Emacs.
 The source used to compile Emacs are taken from Savannah's git
 repository at `https://git.savannah.gnu.org/git/emacs.git' or
-`git://git.savannah.gnu.org/emacs.git'
+`git://git.savannah.gnu.org/emacs.git'.
 
 You can also access the Emacs source repository from the website:
-`https://git.savannah.gnu.org/cgit/emacs.git'
+`https://git.savannah.gnu.org/cgit/emacs.git'.
 
 See both `https://emacswiki.org/emacs/EmacsFromGit' and
-`https://savannah.gnu.org/projects/emacs' for further
-information.")
+`https://savannah.gnu.org/projects/emacs' for further information.")
 
 (provide 'emacs-git-version)
 
