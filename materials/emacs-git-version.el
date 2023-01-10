238d718848919218615e68b4d43b578ecdfa7871
Eason0210 <aqua0210@163.com>
2023-01-10 19:54:08 +0800

    Update emacs-git-version.el

--- materials/emacs-git-version.el
+++ materials/emacs-git-version.el
@@ -1,13 +1,12 @@
-;;; emacs-git-version.el ---
+;;; emacs-git-version.el --- record git commit of Emacs -*- lexical-binding: t -*-
 
 ;; Copyright © 2012 Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 
 ;; Original Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
-;; Modified: Colin Mclear
-;; Keywords: emacs,
+;; Modified: Eason Huang
+;; Keywords: emacs
 ;; Created: 2012-09-21;
-;; Last changed: 12-03-2021 14:47:57
-;; License: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
+;; Last changed: 2023-01-10 20:09:00
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -20,15 +19,18 @@
 (defconst emacs-version-git-commit "@@GIT_COMMIT@@"
   "String giving the git sha1 from which this Emacs was built.
 
-SHA1 of git commit that was used to compile this version of
-emacs. The source used to compile emacs are taken from Savannah's
-git repository at `http://git.savannah.gnu.org/r/emacs.git' or
-git://git.savannah.gnu.org/emacs.git
+SHA1 of git commit that was used to compile this version of Emacs.
+The source used to compile Emacs are taken from Savannah's git
+repository at `https://git.savannah.gnu.org/git/emacs.git' or
+`git://git.savannah.gnu.org/emacs.git'
 
-See both `http://emacswiki.org/emacs/EmacsFromGit' and
-`http://savannah.gnu.org/projects/emacs' for further
+You can also access the Emacs source repository from the website:
+`https://git.savannah.gnu.org/cgit/emacs.git'
+
+See both `https://emacswiki.org/emacs/EmacsFromGit' and
+`https://savannah.gnu.org/projects/emacs' for further
 information.")
 
 (provide 'emacs-git-version)
 
-;; emacs-git-version.el ends here
+;;; emacs-git-version.el ends here
