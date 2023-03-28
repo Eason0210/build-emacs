fe4be4ed8cade84e4a2718734eafc593f5b07f18
Eason0210 <aqua0210@163.com>
2023-03-28 23:39:32 +0800

    Use savchenkovaleriy-big-sur.icns for macOS

--- build-emacs.sh
+++ build-emacs.sh
@@ -295,7 +295,7 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
     # Copy new icon to emacs (currently using a big sur icon)
     # See https://github.com/d12frosted/homebrew-emacs-plus/issues/419
     # cp "${ROOT_DIR}/materials/icons/emacs-big-sur.icns" "${RES_DIR}/Emacs.icns"
-    cp "${ROOT_DIR}/materials/icons/retro-gnu-meditate-levitate.icns" "${RES_DIR}/Emacs.icns"
+    cp "${ROOT_DIR}/materials/icons/savchenkovaleriy-big-sur.icns" "${RES_DIR}/Emacs.icns"
 
     echo "DONE!"
 fi
